a99f5709b5ca05e229517d7984cb4b0e158bcb26
Diego <diego.guisosi@gmail.com>
2018-05-02 23:25:13 -0300

    Add LongSummer benchmark

---
+++ microbenchmark/src/main/java/microbenchmark/LongSummer.java
@@ -0,0 +1,14 @@
+package microbenchmark;
+
+public class LongSummer {
+
+	private long result;
+
+	public long sum(long numberOfElements) {
+		for (int i = 0; i < numberOfElements; i++) {
+			result += i;
+		}
+		return result;
+	}
+
+}
---
+++ microbenchmark/src/test/java/microbenchmark/LongSummerBenchmarkTest.java
@@ -0,0 +1,33 @@
+package microbenchmark;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LongSummerBenchmarkTest {
+
+	private static final long NUMBER_OF_ELEMENTS = 1000000000000L;
+	private LongSummer summer;
+
+	@Before
+	public void setup() {
+		summer = new LongSummer();
+	}
+
+	@Test
+	public void shouldSumNFirstElements() {
+		testSummer(true);
+		testSummer(false);
+	}
+
+	private void testSummer(boolean isWarmup) {
+		long begin = System.currentTimeMillis();
+		long sum = summer.sum(NUMBER_OF_ELEMENTS);
+		if (!isWarmup) {
+			long end = System.currentTimeMillis();
+			System.out.println("Elapsed time: " + (end - begin));
+			System.out.println("Sum: " + sum);
+		}
+
+	}
+
+}
