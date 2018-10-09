0fc079a93acbf1b30c417b806c1e17b4ce3e9398
Diego <diego.guisosi@gmail.com>
2018-10-09 20:06:35 -0300

    modificacao de nomes de tabelas e colunas

--- springboot_hibernate/dirtycheck/src/main/resources/databases/database/replicate_limit.py
+++ springboot_hibernate/dirtycheck/src/main/resources/databases/database/replicate_limit.py
@@ -5,19 +5,19 @@ import sys
 
 FILE_NAME = "insert_limit.sql"
 PROFILE_SEQ = "::PRFE_ID_SEQ::"
-LIMIT_SEQ = "::LMTE_ID_SEQ::"
+CONF_SEQ = "::CNFE_ID_SEQ::"
 
 
 def main(num_profiles, num_limits):
     template_lines = load_template()
 
     with open(FILE_NAME, 'wt') as target:
-        for seq_limit in range(0, num_limits):
+        for seq_conf in range(0, num_limits):
             for seq_profile in range(0, num_profiles):
                 for template_line in template_lines:
                     replaced_line = template_line\
                         .replace(PROFILE_SEQ, "{}".format(seq_profile + 1))\
-                        .replace(LIMIT_SEQ, "{}".format(seq_limit + 1))
+                        .replace(CONF_SEQ, "{}".format(seq_conf + 1))
                     target.write(replaced_line)
                     if (num_profiles + 1) % 100 == 0:
                         write_commit(target)
@@ -37,4 +37,4 @@ def write_commit(target):
 
 if __name__ == '__main__':
     main(int(sys.argv[1]),  # Number of profiles
-         int(sys.argv[2]))  # Number of limits per profile
+         int(sys.argv[2]))  # Number of configurations per profile
